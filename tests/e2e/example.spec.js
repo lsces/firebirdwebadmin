f573e73ac63875dbc72fd0b6012a815ee9832ef5
Popa Adrian Marius <mapopa@gmail.com>
2026-02-24 16:43:19 +0200

    feat(testing): Add Playwright E2E tests with GitHub Actions

---
+++ tests/e2e/example.spec.js
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/database.php');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Firebird Web Admin \/ Database/);
+});
+
+test('login form is visible', async ({ page }) => {
+    await page.goto('/database.php');
+
+    // Expect the login form to be visible
+    await expect(page.locator('form[action="database.php"]')).toBeVisible();
+    await expect(page.getByLabel('Username')).toBeVisible();
+    await expect(page.getByLabel('Password')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+});
